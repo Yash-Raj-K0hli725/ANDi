b2b708c4618fc6028355b878bd96f45fbe4a7648
Yash Raj Kohli <yarajkohli@gmail.com>
2025-04-13 22:33:33 +0530

    ViewInset Handling

--- app/src/main/java/com/example/andi/MainActivity.kt
+++ app/src/main/java/com/example/andi/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.andi
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.andi.databinding.ActivityMainBinding
@@ -17,11 +20,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         bind = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(bind.main){v , inset->
+            val navBar = inset.getInsets(WindowInsetsCompat.Type.navigationBars()).bottom
+            v.updatePadding(bottom = navBar)
+            inset
+        }
         mainvm = ViewModelProvider(this, vmFactory(this))[mainModel::class.java]
         bind.host.adapter = mainvm.vpAdapter
         TabLayoutMediator(bind.tabs, bind.host) { tabs, position ->
             tabs.text = mainvm.tabsName[position]
         }.attach()
     }
-
 }
